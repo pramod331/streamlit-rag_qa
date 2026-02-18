b7332ffb25f2f4af7685b53f2aeaffb392343a85
pramod331 <88658135+pramod331@users.noreply.github.com>
2026-02-18 13:37:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,31 +17,26 @@ st.title("Document RAG Application - Pramod")
 uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
 if uploaded_file is not None:
-    #define save path
-    save_path = os.path.join(working_dir,uploaded_file.name)
-    # save the file
-    with open(save_path,"wb") as f:
+    save_path = os.path.join(working_dir, uploaded_file.name)
+
+    with open(save_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
 
-    # process_document = process_document_to_chroma_db(uploaded_file.name)
     with st.spinner("Processing document..."):
-    process_document_to_chroma_db(uploaded_file.name)
+        process_document_to_chroma_db(uploaded_file.name)
 
+    st.success("Document processed!")
 
-    st.info("Document Processed Successfully")
 
 #text widget to get user input
 user_question = st.text_area("Ask your question about the document")
 
-if st.button("Answer"):
-
-    # answer = answer_question(user_question)
+if st.button("Answer") and user_question:
     with st.spinner("Thinking..."):
-    answer = answer_question(user_question)
-
-
-    st.markdown("###  Llama-3.3-70B Response")
+        answer = answer_question(user_question)
 
     st.markdown(answer)
+
+
 
 
