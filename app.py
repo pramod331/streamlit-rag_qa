0ea3a893ea16b226f3ba823871131fb5b23e5e63
pramod331 <88658135+pramod331@users.noreply.github.com>
2026-02-18 11:10:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+
+import streamlit as st
+import certifi
+# print(certifi.where())
+
+
+from rag_utility import process_document_to_chroma_db, answer_question
+
+# set the working directory
+# working_dir = os.getcwd()
+working_dir = os.path.dirname(os.path.abspath((__file__)))
+
+st.title(" Llama-3.3-70B - Document RAG - Pramod")
+
+#file uploader widget
+uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+
+if uploaded_file is not None:
+    #define save path
+    save_path = os.path.join(working_dir,uploaded_file.name)
+    # save the file
+    with open(save_path,"wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    process_document = process_document_to_chroma_db(uploaded_file)
+    st.info("Document Processed Successfully")
+
+#text widget to get user input
+user_question = st.text_area("Ask your question about the document")
+
+if st.button("Answer"):
+
+    answer = answer_question(user_question)
+
+    st.markdown("###  Llama-3.3-70B Response")
+    st.markdown(answer)
---
+++ rag_utility.py
@@ -0,0 +1,78 @@
+import os
+
+from http.client import responses
+
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from langchain_huggingface import HuggingFaceEmbeddings
+
+from langchain_chroma import Chroma
+from langchain_groq import ChatGroq
+from langchain_classic.chains import RetrievalQA
+
+# load environment variables
+load_dotenv()
+
+# setup working directory of the current file, to identify the files in the same location
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+#load embedding model
+embedding = HuggingFaceEmbeddings()
+
+# embeddings = HuggingFaceEmbeddings(
+#     model_name="sentence-transformers/all-mpnet-base-v2"
+# )
+
+vec = embeddings.embed_query("Hello world")
+print(len(vec))
+
+
+# Load the Llama-3.3-70B model from Groq
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0
+)
+
+def process_document_to_chroma_db(file_name):
+    #Load the PDF document using UnstructuredPDFLoader
+    loader = UnstructuredPDFLoader(f"{working_dir}/{file_name}")
+    documents = loader.load()
+    #split the text into chunks for embedding
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=2000,
+        chunk_overlap=200
+    )
+    texts = text_splitter.split_documents(documents)
+
+    #store the documents chunks into chroma vector database
+    vectordb= Chroma.from_documents(
+        documents=texts,
+        embeddings=embedding,
+        persist_directory=f"{working_dir}/doc_vectorstore"
+    )
+    return 0
+
+def answer_question(user_question):
+    # Load the persistent Chroma vector database
+    vectordb = Chroma(
+        persist_directory=f"{working_dir}/doc_vectorstore",
+        embedding_function=embedding
+    )
+    # Create a retriever for document view
+    retriever = vectordb.as_retriever()
+
+    #create a RetrieverQA chain to answer user questions using Llama-3.3-70B
+
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever
+    )
+
+    response = qa_chain.invoke({"query": user_question})
+    answer = response["result"]
+
+    return answer
