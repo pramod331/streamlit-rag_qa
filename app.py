1601e72f9d53f595042ce06df502804006602d7a
pramod331 <88658135+pramod331@users.noreply.github.com>
2026-04-09 16:46:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,21 +16,33 @@ st.title("Document RAG Application - Pramod")
 
 # File uploader widget
 uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
-
 if uploaded_file is not None:
     # Save the uploaded file to a temporary location
     temp_file = os.path.join(working_dir, "temp.pdf")
     with open(temp_file, "wb") as f:
         f.write(uploaded_file.getbuffer())
 
-    try:
-        # Process the document
+    # Check if the file exists
+    if os.path.exists(temp_file):
         with st.spinner("Processing document..."):
             process_document_to_chroma_db(temp_file)
-
         st.success("Document processed!")
-    except Exception as e:
-        st.error(f"Error processing document: {e}")
+    else:
+        st.error("File not found")
+# if uploaded_file is not None:
+#     # Save the uploaded file to a temporary location
+#     temp_file = os.path.join(working_dir, "temp.pdf")
+#     with open(temp_file, "wb") as f:
+#         f.write(uploaded_file.getbuffer())
+
+#     try:
+#         # Process the document
+#         with st.spinner("Processing document..."):
+#             process_document_to_chroma_db(temp_file)
+
+#         st.success("Document processed!")
+#     except Exception as e:
+#         st.error(f"Error processing document: {e}")
 
 # Text widget to get user input
 user_question = st.text_area("Ask your question about the document")
